47bd08250f7e8b48a3419ca2033c45325b0adee7
Малаховский Игорь <megazord29@yandex.ru>
2023-01-29 00:08:21 +0400

    Перевод модуля создания персонажа на ООП ч.2

--- character_creation_module/main.py
+++ character_creation_module/main.py
@@ -61,6 +61,61 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита'
 
 
-warrior = Warrior('Кодослав')
-print(warrior)
-print(warrior.special())
+def choice_char_class(char_name: str) -> Character:
+    """
+    Возвращает строку с выбранным
+    классом персонажа.
+    """
+    game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
+
+    approve_choice: str = None
+
+    while approve_choice != 'y':
+        selected_class = input(
+                            'Введи название персонажа, '
+                            'за которого хочешь играть: Воитель — warrior, '
+                            'Маг — mage, Лекарь — healer: '
+                            )
+        char_class: Character = game_classes[selected_class](char_name)
+        print(char_class)
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
+    return char_class
+
+
+def start_training(character: Character) -> str:
+    """
+    Принимает на вход имя и класс персонажа.
+    Возвращает сообщения о результатах цикла тренировки персонажа.
+    """
+    commands = {
+            "attack": character.attack,
+            "defence":  character.defence,
+            "special": character.special,
+            }
+
+    # Замените конструкцию условных операторов на словарь.
+    print('Потренируйся управлять своими навыками.')
+    print(
+        'Введи одну из команд: attack — чтобы атаковать противника, '
+        'defence — чтобы блокировать атаку противника или '
+        'special — чтобы использовать свою суперсилу.'
+        )
+    print('Если не хочешь тренироваться, введи команду skip.')
+    cmd = None
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        # Вместо блока условных операторов добавьте условие
+        # принадлежности введённой команды словарю.
+        # В функции print() будет вызываться метод класса,
+        # который соответствует введённой команде.
+        if cmd in commands:
+            print(commands[cmd]())
+    return 'Тренировка окончена.'
+
+if __name__ == "__main__":
+    warrior = Warrior('Кодослав')
+    print(warrior)
+    print(warrior.special())
+    start_training(warrior)
